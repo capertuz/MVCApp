3fd521587cf855ca1811630a6b7d0f21cba5913f
capertuz <capertuz@gmail.com>
2019-05-30 12:54:09 -0400

    Update Data posted in the customer form

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -58,9 +58,22 @@ namespace Vidly.Controllers
         }
 
         [HttpPost]
-        public ActionResult Create(Customer customer)
+        public ActionResult Save(Customer customer)
         {
-            _context.Customers.Add(customer);
+            if (customer.Id == 0)
+            {
+                _context.Customers.Add(customer);
+            }
+            else
+            {
+                var customerInDb = _context.Customers.Single(c => c.Id == customer.Id);
+
+                customerInDb.MembershipTypeId = customer.MembershipTypeId;
+                customerInDb.Birthdate = customer.Birthdate;
+                customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+                customerInDb.Name = customer.Name;
+
+            }
             _context.SaveChanges();
 
             return RedirectToAction("List","Customers");
